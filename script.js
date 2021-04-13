48c2b1af5b75af32dceac4366972b4d2a2fd57cf
heyset <c.inaciomatheus@gmail.com>
2021-04-13 19:27:49 -0300

    Adiciona funcao que escolhe cor aleatoria como resposta

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const circleContainer = document.getElementById('color-array');
+const circleArray = circleContainer.children;
 
 function getRandomRGBColorString() {
   let finalString = '(';
@@ -26,13 +27,38 @@ function createOptions(numberOfOptions) {
 }
 
 function assignRandomColors() {
-  const circleArray = circleContainer.children;
-
   for (let i = 0; i < circleArray.length; i += 1) {
     const rgbString = getRandomRGBColorString();
     circleArray[i].style.backgroundColor = 'rgb' + rgbString;
   }
 }
 
+function getRandomIndex() {
+  const max = circleArray.length;
+
+  const randomIndex = Math.floor(Math.random() * max);
+  return randomIndex;
+}
+
+function getRGBStringFromCircle(index) {
+  const bgColor = circleArray[index].style.backgroundColor;
+  const rgbString = bgColor.replace('rgb', '');
+
+  return rgbString;
+}
+
+let answerElement;
+
+function assignRandomAnswer() {
+  const answerIndex = getRandomIndex();
+  const rgbString = getRGBStringFromCircle(answerIndex);
+
+  answerElement = circleArray[answerIndex];
+  const questionString = document.getElementById('rgb-color');
+
+  questionString.innerText = rgbString;
+}
+
 createOptions(6);
 assignRandomColors();
+assignRandomAnswer();
