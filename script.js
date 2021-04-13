0695406ef64ae4f2367ffa835b4414a7d94864c2
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-13 17:58:27 -0300

    requisito 6 feito

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 const getP = document.querySelector('#rgb-color');
-const getBall = document.getElementsByClassName('ball')
+const getBall = document.getElementsByClassName('ball');
+const getColorOptions = document.getElementById('color-options');
+const getAnswer = document.getElementById('answer');
+const getReset = document.getElementById('reset-game');
 
 function mathRandom() {
   return Math.floor(Math.random() * 255);
 }
 
 function randomColorBalls() {
-  for(let index = 0; index < getBall.length; index += 1) {
+  for (let index = 0; index < getBall.length; index += 1) {
     getBall[index].style.backgroundColor = `rgb(${mathRandom()}, ${mathRandom()}, ${mathRandom()})`;
   }
 }
 randomColorBalls();
 
 function rgbColorParagraph() {
-  const color = Math.floor(Math.random() * 6)
-  getP.innerHTML = getBall[color].style.backgroundColor
+  const color = Math.floor(Math.random() * 6);
+  getP.innerHTML = getBall[color].style.backgroundColor;
 }
 rgbColorParagraph();
+
+function rightOrWrong() {
+  getColorOptions.addEventListener('click', (event) => {
+    if (getP.innerText === event.target.style.backgroundColor) {
+      getAnswer.innerHTML = 'Acertou!';
+    } if (getP.innerText !== event.target.style.backgroundColor) {
+      getAnswer.innerHTML = 'Errou! Tente novamente!';
+    }
+  });
+}
+rightOrWrong();
+
+function resetGame() {
+  getReset.addEventListener('click', () => {
+    randomColorBalls()
+    rgbColorParagraph()
+    getAnswer.innerHTML = 'Escolha uma cor'
+  })
+}
+resetGame()
