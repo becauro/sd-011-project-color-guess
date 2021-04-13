8ad9575d4b425466cfcdcad6eb5ba21ca0c2d68a
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-04-13 18:41:04 -0300

    Lint

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ ballColorGenerator();
 function randomTextColor() {
   const randomColor = Math.round(Math.random() * 5);
   correctColor.innerText = balls[randomColor].style.backgroundColor;
-};
+}
 randomTextColor();
 
 ballContainer.addEventListener('click', (event) => {
