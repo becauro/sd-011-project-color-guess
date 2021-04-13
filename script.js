ed4207734774eb89352afa49510f1998883dca60
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-04-13 16:01:40 -0300

    Ball generator

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ const randomRgbColor = () => {
   let color3 = Math.round(Math.random() * 255);
   return `rgb(${color1}, ${color2}, ${color3})`;
 };
+
+const ballColorGenerator = () => {
+  for (let i = 0; i < balls.length; i += 1) {
+    balls[i].style.backgroundColor = randomRgbColor();
+  }
+};
+ballColorGenerator();
