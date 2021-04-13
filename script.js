e2b925fdabf1c834ff70e9f13066eb00e68bc808
jhenriquemargrafm <jh.margraf@gmail.com>
2021-04-13 19:17:45 -0300

    Requisito 4: Ajuste das cores com o texto

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-// Requisito 2 - Escrever cor no formato RGB
+// Requisito 2 e 3 - Escrever cor no formato RGB  e Sorteio das
 const colorToBeGuessed = document.querySelector('#rgb-color');
-
-function drawNumbers() {
-  const x = Number(Math.floor(Math.random() * 256));
-  const y = Number(Math.floor(Math.random() * 256));
-  const z = Number(Math.floor(Math.random() * 256));
-  const color = `rgb(${x}, ${y}, ${z})`;
-  colorToBeGuessed.innerText = `${color},`;
-}
-drawNumbers();
 
 function colorBalls() {
   const balls = document.querySelectorAll('.ball');
@@ -16,7 +7,9 @@ function colorBalls() {
     const x = Number(Math.floor(Math.random() * 256));
     const y = Number(Math.floor(Math.random() * 256));
     const z = Number(Math.floor(Math.random() * 256));
-    balls[index].style.backgroundColor = `rgb( ${x}, ${y}, ${z} )`;
+    balls[index].style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
   }
+  const drawNumber = Number(Math.floor(Math.random() * 5));
+  colorToBeGuessed.innerText = balls[drawNumber].style.backgroundColor;
 }
 colorBalls();
