0c877a15e1e076242558f10e4193af84ff3e3027
heyset <c.inaciomatheus@gmail.com>
2021-04-13 19:12:32 -0300

    Corrige cor aleatoria para ser justa com o 255 e designa cores aleatorias

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const circleContainer = document.getElementById('color-array');
+
 function getRandomRGBColorString() {
   let finalString = '(';
 
   for (let i = 0; i < 3; i += 1) {
-    const randomTone = Math.round(Math.random() * 255);
+    const randomTone = Math.floor(Math.random() * 256);
 
     finalString += randomTone.toString(10);
     if (i !== 2) {
@@ -14,4 +16,23 @@ function getRandomRGBColorString() {
   return finalString;
 }
 
-console.log(getRandomRGBColorString());
+function createOptions(numberOfOptions) {
+  for (let i = 0; i < numberOfOptions; i += 1) {
+    const newOption = document.createElement('li');
+    newOption.classList.add('ball');
+
+    circleContainer.appendChild(newOption);
+  }
+}
+
+function assignRandomColors() {
+  const circleArray = circleContainer.children;
+
+  for (let i = 0; i < circleArray.length; i += 1) {
+    const rgbString = getRandomRGBColorString();
+    circleArray[i].style.backgroundColor = 'rgb' + rgbString;
+  }
+}
+
+createOptions(6);
+assignRandomColors();
