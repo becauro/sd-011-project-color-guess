6483af1005ee36a786adb2f7f51bd2d77c20e9d8
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-13 17:09:52 -0300

    requisito 3 e 4 feitos

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const getP = document.querySelector('#rgb-color');
+const getBall = document.getElementsByClassName('ball')
 
 function mathRandom() {
   return Math.floor(Math.random() * 255);
 }
 
-function rgbColor(value) {
-  getP.innerHTML = value;
+function randomColorBalls() {
+  for(let index = 0; index < getBall.length; index += 1) {
+    getBall[index].style.backgroundColor = `rgb(${mathRandom()}, ${mathRandom()}, ${mathRandom()})`;
+  }
 }
-rgbColor(`rgb(${mathRandom()}, ${mathRandom()}, ${mathRandom()})`);
+randomColorBalls();
+
+function rgbColorParagraph() {
+  const color = Math.floor(Math.random() * 6)
+  getP.innerHTML = getBall[color].style.backgroundColor
+}
+rgbColorParagraph();
