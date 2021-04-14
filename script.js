022a9521d37bd91cac8e867fe38fb196bdd017f7
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-14 16:32:41 -0300

    Passo 6

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
 const color = document.getElementsByClassName('ball');
+const rgbColor = document.getElementById('rgb-color');
 
 function randomColor() {
   const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
 
-  const msg = `rgb(${r}, ${g}, ${b})`;
+  const msg = `(${r}, ${g}, ${b})`;
   return msg;
 }
 
-function ballColor() {
-  color[0].style.backgroundColor = 'rgb(168, 34, 1)';
-
-  for (let i = 1; i < color.length; i += 1) {
-    color[i].style.backgroundColor = randomColor();
-  }
+function randomRgb() {
+  rgbColor.innerText = randomColor();
 }
 
-function checkRepeatColor() {
+function ballColor() {
+  const randomPosition = Math.floor(Math.random() * 6);
+  const rgbColorComplete = `rgb${rgbColor.innerText}`;
+  console.log(rgbColorComplete)
+
   for (let i = 0; i < color.length; i += 1) {
-    let j = 1;
-    if (color[i].style.backgroundColor === color[j].style.backgroundColor) {
-      color[i].style.backgroundColor = randomColor();
-    }
-    j += 1;
+    color[i].style.backgroundColor = `rgb${randomColor()}`;
   }
+
+  color[randomPosition].style.backgroundColor = rgbColorComplete;
 }
 
 function checkCorrect() {
-  const rgbColor = document.getElementById('rgb-color');
+  const p = document.getElementById('answer');
   const rgbColorComplete = `rgb${rgbColor.innerText}`;
-  let p = document.getElementById('answer');
+  p.innerText = 'Escolha uma cor';
 
-  for (let i = 0; i < color.length; i += 1){
+  for (let i = 0; i < color.length; i += 1) {
     color[i].addEventListener('click', () => {
       if (color[i].style.backgroundColor === rgbColorComplete) {
         p.innerText = 'Acertou!';
@@ -43,6 +42,16 @@ function checkCorrect() {
   }
 }
 
+function resetGame() {
+  const button = document.getElementById('reset-game');
+
+  button.addEventListener('click', () => {
+    randomRgb();
+    ballColor();
+    checkCorrect();
+  })
+}
+
 ballColor();
-checkRepeatColor();
 checkCorrect();
+resetGame()
