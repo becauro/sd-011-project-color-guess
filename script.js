46c8939539fc859f0afcef93db929b8bb28d894d
jhenriquemargrafm <jh.margraf@gmail.com>
2021-04-13 21:42:56 -0300

    Requisito 5:teste

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Requisito 2 e 3 - Escrever cor no formato RGB  e Sorteio das
+// Requisito 2 e 4 - Escrever cor no formato RGB  e Sorteio das
 const colorToBeGuessed = document.querySelector('#rgb-color');
+const balls = document.querySelectorAll('.ball');
 
 function colorBalls() {
-  const balls = document.querySelectorAll('.ball');
   for (let index = 0; index < balls.length; index += 1) {
     const x = Number(Math.floor(Math.random() * 256));
     const y = Number(Math.floor(Math.random() * 256));
@@ -13,3 +13,16 @@ function colorBalls() {
   colorToBeGuessed.innerText = balls[drawNumber].style.backgroundColor;
 }
 colorBalls();
+
+function compare() {
+  const circles = document.getElementById('allcircles');
+  circles.addEventListener('click', (event) => {
+    const answer = document.getElementById('answer');
+    if (colorToBeGuessed.innerText !== event.target.style.backgroundColor) {
+      answer.innerText = 'Errou! Tente novamente!';
+    } else {
+      answer.innerText = 'Acertou!';
+    }
+  });
+}
+compare();
