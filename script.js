ae76634f0f75d6ab58e07ed86ff8996c8690fa99
Tales Coelho <tales-coelho@live.com>
2021-04-14 10:49:02 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const balls = document.getElementById('balls');
 const rgbColor = document.getElementById('rgb-color');
+const answer = document.getElementById('answer');
+const resetGame = document.getElementById('reset-game');
+const score = document.getElementById('score');
 
 function generateRamdomColor() {
   const color = [];
   for (let index = 0; index < 3; index++) {
     color[index] = Math.ceil(Math.random() * 255);
   }
-  return `(${color[0]} , ${color[1]} , ${color[2]})`
+  return `rgb(${color[0]}, ${color[1]}, ${color[2]})`
 }
 
 function createDivBall() {
@@ -15,10 +18,35 @@ function createDivBall() {
     const ball = document.createElement('div');
     ball.className = 'ball';
     colors[index] = generateRamdomColor();
-    ball.style.backgroundColor = `rgb${colors[index]}`;
+    ball.style.backgroundColor = `${colors[index]}`;
     balls.appendChild(ball);
   }
   return colors;
 }
 let colors = createDivBall();
-console.log(colors);
+let randnum = Math.ceil(Math.random() * 5);
+rgbColor.innerText = `${colors[randnum]}`;
+
+balls.addEventListener('click', (event) => {
+  eTarget = event.target;
+  if (eTarget.className === 'ball') {
+    console.log(rgbColor.innerText)
+    console.log(eTarget.style.backgroundColor)
+    if (`${eTarget.style.backgroundColor}` === `${rgbColor.innerText}`) {
+      answer.innerText = 'Acertou!'
+      score.innerText = Number(score.innerText) + 3;
+    } else {
+      answer.innerText = 'Errou! Tente novamente!'
+    }
+  }
+});
+
+resetGame.addEventListener('click', () => {
+  answer.innerText = 'Escolha uma cor';
+  for (let index = 0; index < balls.childElementCount;) {
+    balls.removeChild(balls.children[index]);
+  }
+  let colors = createDivBall();
+  let randnum = Math.ceil(Math.random() * 5);
+  rgbColor.innerText = `${colors[randnum]}`;
+});
