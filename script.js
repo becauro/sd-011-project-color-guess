11f9838c02679182ea8f91763de41796b2395702
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-14 15:54:44 -0300

    Passo 5

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
-function randomColor() {
-  let r = Math.floor(Math.random() * 255);
-  let g = Math.floor(Math.random() * 255);
-  let b = Math.floor(Math.random() * 255);
+const color = document.getElementsByClassName('ball');
 
-  let msg = `rgb(${r}, ${g}, ${b})`;
+function randomColor() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  const msg = `rgb(${r}, ${g}, ${b})`;
   return msg;
 }
 
 function ballColor() {
-  const color = document.getElementsByClassName('ball');
+  color[0].style.backgroundColor = 'rgb(168, 34, 1)';
 
-  for (let i = 0; i < color.length; i += 1) {
+  for (let i = 1; i < color.length; i += 1) {
     color[i].style.backgroundColor = randomColor();
   }
 }
 
 function checkRepeatColor() {
-  const color = document.getElementsByClassName('ball');
-
   for (let i = 0; i < color.length; i += 1) {
-    for (let j = 1; j < color.length; j += 1) {
-      if (color[i].style.backgroundColor === color[j].style.backgroundColor) {
-        color[i].style.backgroundColor = randomColor();
-      }
+    let j = 1;
+    if (color[i].style.backgroundColor === color[j].style.backgroundColor) {
+      color[i].style.backgroundColor = randomColor();
     }
+    j += 1;
+  }
+}
+
+function checkCorrect() {
+  const rgbColor = document.getElementById('rgb-color');
+  const rgbColorComplete = `rgb${rgbColor.innerText}`;
+  let p = document.getElementById('answer');
+
+  for (let i = 0; i < color.length; i += 1){
+    color[i].addEventListener('click', () => {
+      if (color[i].style.backgroundColor === rgbColorComplete) {
+        p.innerText = 'Acertou!';
+      } else {
+        p.innerText = 'Errou! Tente novamente!';
+      }
+    });
   }
 }
 
 ballColor();
 checkRepeatColor();
+checkCorrect();
