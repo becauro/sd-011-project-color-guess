6243ac72babaeddc9ade8056161b49be4c9103b1
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-04-13 21:53:30 -0300

    adiciona requisito 05

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const ballClass = 'ball';
 
+const objective = document.getElementById('rgb-color');
 const optionsContainer = document.getElementById('options-container');
+const options = document.getElementsByClassName(ballClass);
+const answer = document.getElementById('answer');
 
-const spanElement = document.createElement('span');
+const divElement = document.createElement('div');
 
 function createColorOptions(quantity) {
   for (let index = 0; index < quantity; index += 1) {
-    const newSpan = spanElement.cloneNode();
-    newSpan.classList.add(ballClass);
-    optionsContainer.appendChild(newSpan);
+    const newDiv = divElement.cloneNode();
+    newDiv.classList.add(ballClass);
+    optionsContainer.appendChild(newDiv);
   }
 }
 
@@ -21,16 +24,30 @@ function generateRandomColor() {
 }
 
 function setColorElements() {
-  const elements = document.getElementsByClassName(ballClass);
+  for (let index = 0; index < options.length; index += 1) {
+    options[index].style.backgroundColor = `rgb${generateRandomColor()}`;
+  }
+}
 
-  for (let index = 0; index < elements.length; index += 1) {
-    elements[index].style.backgroundColor = `rgb${generateRandomColor()}`;
+function setObjective() {
+  const randomOption = options[getRandomNumber(0, options.length - 1)];
+  const color = window.getComputedStyle(randomOption).backgroundColor;
+  objective.innerText = color.slice(-(color.length - 3));
+}
+
+function checkAnswer({ target }) {
+  if (target.classList.contains(ballClass)) {
+    if (window.getComputedStyle(target).backgroundColor === `rgb${objective.innerText}`) {
+      answer.innerText = 'Acertou!';
+    } else answer.innerText = 'Errou! Tente novamente!';
   }
 }
 
 function onLoad() {
   createColorOptions(6);
   setColorElements();
+  setObjective();
+  optionsContainer.addEventListener('click', checkAnswer);
 }
 
 window.onload = onLoad;
