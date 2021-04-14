7df529856a171d5bc1f6920b62d47156e7f4884a
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-04-13 23:24:11 -0300

    implementa requisito 5

--- script.js
+++ script.js
@@ -6,16 +6,37 @@ function generateRandomColor() {
   return rgb;
 }
 
+function checkAnswer(event) {
+  const ball = event.target;
+  const answer = ball.style.backgroundColor;
+  const question = document.getElementById('rgb-color').innerText;
+  const result = document.getElementById('answer');
+  if (answer === question) {
+    result.innerText = 'Acertou!';
+  } else {
+    result.innerText = 'Errou! Tente novamente!';
+  }
+}
+
 function createColors() {
   const container = document.getElementById('ball-container');
-  for (let index = 0; index < 5; index += 1) {
+  for (let index = 0; index < 6; index += 1) {
     const ball = document.createElement('div');
     ball.classList.add('ball');
     ball.style.backgroundColor = generateRandomColor();
+    ball.addEventListener('click', checkAnswer);
     container.appendChild(ball);
   }
 }
 
+function getQuestion() {
+  const ballList = document.getElementsByClassName('ball');
+  const question = document.getElementById('rgb-color');
+  const draw = ballList[Math.floor(Math.random() * ballList.length)];
+  question.innerText = draw.style.backgroundColor;
+}
+
 window.onload = function load() {
   createColors();
+  getQuestion();
 };
