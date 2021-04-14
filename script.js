b1682896219dfff688d449043538985a41442427
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-04-14 01:16:13 -0300

    Corrigi erros lint

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
-window.onload = function start(){
-    const circles = document.querySelectorAll('.ball')
+window.onload = function start() {
+  const circles = document.querySelectorAll('.ball');
 
-    for(let index = 0; index<circles.length; index +=1){
-        const color1 = parseInt(Math.random()*255)
-        const color2 = parseInt(Math.random()*255)
-        const color3 = parseInt(Math.random()*255)
-    
-        circles[index].style.backgroundColor = `rgb(${color1}, ${color2}, ${color3})`
+  for (let index = 0; index < circles.length; index += 1) {
+    const color1 = parseInt(Math.random() * 255, 10);
+    const color2 = parseInt(Math.random() * 255, 10);
+    const color3 = parseInt(Math.random() * 255, 10);
+    circles[index].style.backgroundColor = `rgb(${color1}, ${color2}, ${color3})`;
+  }
+  const number = parseInt(Math.random() * 6, 10);
+  const colorTarget = document.querySelector('#rgb-color');
+  const colorSorted = circles[number].style.backgroundColor;
+  colorTarget.innerHTML = colorSorted;
+};
 
-    }
+const circleClick = document.querySelector('#colors');
+const result = document.querySelector('#answer');
+const score = document.querySelector('#score');
+let counter = 0;
 
-    const number = parseInt(Math.random()*6)
-    const colorTarget = document.querySelector('#rgb-color')
-    const colorSorted = circles[number].style.backgroundColor
-    colorTarget.innerHTML = colorSorted
-
+function check(event) {
+  const colorTarget = document.querySelector('#rgb-color');
+  const colorSelected = event.target;
+  if (colorSelected.style.backgroundColor === colorTarget.innerText) {
+    result.innerText = 'Acertou!';
+    counter += 3;
+    score.innerText = `${counter}`;
+  } else {
+    result.innerText = 'Errou! Tente novamente!';
+  }
 }
 
-const circleClick = document.querySelector('#colors')
-const result = document.querySelector('#answer')
-const score = document.querySelector('#score')
-let counter = 0
+circleClick.addEventListener('click', check);
 
-circleClick.addEventListener('click', function(event){
-  const colorTarget = document.querySelector('#rgb-color')
-  const colorSelected = event.target
-  
-  if(colorSelected.style.backgroundColor === colorTarget.innerText){
-    result.innerText = 'Acertou!'
-    counter +=3
-    score.innerText = `${counter}`
-  } else {
-    result.innerText = 'Errou! Tente novamente!'
-  }
-})
+const buttonReset = document.querySelector('#reset-game');
 
-const buttonReset = document.querySelector('#reset-game')
+function restart() {
+  window.onload();
+  result.innerText = 'Escolha uma cor';
+}
 
-buttonReset.addEventListener('click', function(){
-  window.onload()
-  const result = document.querySelector('#answer')
-  result.innerText = 'Escolha uma cor'
-})
+buttonReset.addEventListener('click', restart);
