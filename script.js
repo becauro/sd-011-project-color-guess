0ac5ede65bf1613db282f52433500fc29092c475
Marco Galindo <marco1408@gmail.com>
2021-04-15 13:19:48 -0300

    requisito 3 finalizado

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+// 2
+
 const rgbColor = document.getElementById('rgb-color');
-window.onload = function generateRandomColor() {
+function generateRandomColor() {
   const redCode = Math.floor(Math.random() * 255);
   const greenCode = Math.floor(Math.random() * 255);
   const blueCode = Math.floor(Math.random() * 255);
   const rgbCode = `(${redCode}, ${greenCode}, ${blueCode})`;
   rgbColor.innerText = rgbCode;
-};
+}
+window.addEventListener('load', generateRandomColor);
+
+// 3
+
+const ballContainer = document.getElementById('ball-container');
+function generateBalls() {
+  for (let index = 0; index < 6; index += 1) {
+    const ball = document.createElement('div');
+    ball.className = 'ball';
+    ballContainer.appendChild(ball);
+  }
+}
+window.addEventListener('load', generateBalls);
