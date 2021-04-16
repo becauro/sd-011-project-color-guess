bc36a18a8610f3ff3f8dc0dd02ed071920ba254e
Alexandre <naitwa.alexandre@gmail.com>
2021-04-16 11:54:22 -0300

    05.01

---
+++ script.js
@@ -0,0 +1,55 @@
+function addBalls() {
+  const element = document.getElementById('wrapper-balls');
+  const amountOfBalls = 6;
+  for (let i = 0; i < amountOfBalls; i += 1) {
+    const div = document.createElement('div');
+    div.classList.add('ball');
+    element.appendChild(div);
+  }
+}
+addBalls();
+
+function randomRgb() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+function getBackgroundColor() {
+  const balls = document.querySelectorAll('.ball');
+  for (let i = 0; i < balls.length; i += 1) {
+    balls[i].style.background = randomRgb();
+  }
+}
+getBackgroundColor();
+
+function getBackGroundValue() {
+  const balls = document.querySelectorAll('.ball');
+  const prop = [];
+  for (let i = 0; i < balls.length; i += 1) {
+    prop.push(balls[i].style.background);
+  }
+  const result =  prop.sort();
+  return result[0];
+}
+// console.log(getBackGroundValue());
+
+function handleRgbValue() {
+  const element = document.getElementById('rgb-color');
+  element.innerHTML = getBackGroundValue().replace('rgb', "");
+}
+handleRgbValue();
+
+function clickBalls() {
+  const balls = document.querySelectorAll('.ball');
+  for (let i = 0; i < balls.length; i += 1) {
+    balls[i].addEventListener('click', trueOrFalse);
+  }
+}
+clickBalls();
+
+function trueOrFalse(event) {
+  // compares the color with the current paragrah and true or false.
+  console.log(event.target.style.background);
+}
