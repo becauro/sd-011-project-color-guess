e7f19da5594b5f081e77a0624be05973537cfcb0
Diego Ribeiro <dihrm2001@gmail.com>
2021-04-17 00:50:53 -0300

    Requisito 3 e 4

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const HINT_RGB = document.getElementById('rgb-color');
 
 function randomRgbColor() {
-  let colors = [];
+  const COLORS = [];
   for (let index = 0; index < 3; index += 1) {
-    colors.push(Math.floor(Math.random() * 256));
+    COLORS.push(Math.floor(Math.random() * 256));
   }
-  colors = `rgb (${colors[0]}, ${colors[1]}, ${colors[2]})`;
-  return colors;
+  const RANDOM_COLORS = `rgb(${COLORS[0]},${COLORS[1]},${COLORS[2]})`;
+  return RANDOM_COLORS;
 }
 
-window.onload = function() {
-  HINT_RGB.innerText = randomRgbColor();
+const WIN_INDEX = Math.floor(Math.random() * 6);
+
+function gameColors() {
+  let winColor;
+  for (let index = 0; index < 6; index += 1) {
+    const NEW_COLOR = document.createElement('div');
+    NEW_COLOR.className = 'ball';
+    NEW_COLOR.style.backgroundColor = randomRgbColor();
+    document.getElementById('game-colors').appendChild(NEW_COLOR);
+    if (index === WIN_INDEX) {
+      winColor = NEW_COLOR.style.backgroundColor;
+    }
+  }
+  return winColor;
 }
+
+window.onload = () => {
+  const WINNER = gameColors();
+  const END = WINNER.length;
+  HINT_RGB.innerText = WINNER.substring(3, END);
+};
