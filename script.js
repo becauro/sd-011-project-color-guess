54b1945293ba2b254bbc19c62492d657dbf2b795
Matheus Gaspar <msouzagaspar@gmail.com>
2021-04-17 21:23:00 -0300

    requisitos 3 e 4: adiciona opções de cores

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const colorCodeElement = document.getElementById('rgb-color');
+const colorOptionsContainerElement = document.getElementById('color-options');
 
 function randomRGBNumber() {
   return Math.floor(Math.random() * 256);
@@ -11,4 +12,34 @@ function generateRandomRGBColor() {
   return `(${randomR}, ${randomG}, ${randomB})`;
 }
 
+function generateColorOptions() {
+  const numberOfOptions = 6;
+  const correctGuessIndex = Math.floor(Math.random() * numberOfOptions);
+  const options = [];
+  for (let index = 0; index < numberOfOptions; index += 1) {
+    if (index === correctGuessIndex) {
+      options[index] = colorCodeElement.textContent;
+    } else {
+      options[index] = generateRandomRGBColor();
+    }
+  }
+  return options;
+}
+
+function makeColorOptionsElements(rgbCodes) {
+  return rgbCodes.map((rgbCode) => {
+    const element = document.createElement('div');
+    element.classList.add('ball');
+    element.style.backgroundColor = `rgb${rgbCode}`;
+    return element;
+  });
+}
+
+function appendColorOptionsElements(elements) {
+  for (let index = 0; index < elements.length; index += 1) {
+    colorOptionsContainerElement.appendChild(elements[index]);
+  }
+}
+
 colorCodeElement.textContent = generateRandomRGBColor();
+appendColorOptionsElements(makeColorOptionsElements(generateColorOptions()));
