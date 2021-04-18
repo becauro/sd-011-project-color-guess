ee2314f5f7694d7ee8942a9ebc4eb4f44121076d
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-04-18 19:14:00 -0300

    Adiciona requisito 5

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const balls = document.querySelectorAll('.ball');
+const answerRGB = document.querySelector('#rgb-color');
+const answer = document.querySelector('#answer');
+const options = document.querySelector('#options');
 
 // Função para gerar números RGB aleatórios;
 function randomRGBNumber() {
@@ -26,3 +29,34 @@ function addRGBColor() {
 }
 
 window.onload = addRGBColor;
+
+// Escolhe uma das cores aleatórias para colocar no título
+
+function changeTitleQuestion() {
+  const randomNumber = Math.floor(Math.random() * 5 + 1);
+  let randomTitle = balls[randomNumber].style.backgroundColor;
+  randomTitle = randomTitle.replaceAll('rgb', '');
+  answerRGB.innerText = randomTitle;
+}
+
+// Verifica se acertou a cor
+
+function checkAnswer(event) {
+  let titleRGBValue = answerRGB.innerText;
+  titleRGBValue = `rgb${titleRGBValue}`;
+
+  if (event.target.style.backgroundColor === titleRGBValue) {
+    answer.innerText = 'Acertou!';
+  } else {
+    answer.innerText = 'Errou! Tente novamente!';
+  }
+}
+
+options.addEventListener('click', checkAnswer);
+
+// Adiciona funções para iniciar com o carregamento da página
+
+window.onload = function start() {
+  addRGBColor();
+  changeTitleQuestion();
+};
