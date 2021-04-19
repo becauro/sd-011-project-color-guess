bcd4535044b1c829c9b5d95ab66f1890442ac442
PH Assis <pieit@live.com>
2021-04-18 21:49:42 -0300

    exercise 5 ready

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const colorBalls = document.querySelector('#color-palette').children;
+const rgbColor = document.querySelector('#rgb-color');
+const colorSelectionH2 = document.querySelector('#color-selection h2');
 
 function randomColor() {
   const random = 'rgb(X,X,X)';
@@ -19,4 +21,25 @@ function randomColor() {
   }
 }());
 
-let xarp = 2;
+function randomCorrect() {
+  return Math.floor(Math.random() * 6);
+}
+
+colorBalls[randomCorrect()].style.backgroundColor = 'rgb( 168, 34, 1)';
+
+function checkAnswer() {
+  for (let i = 0; i < colorBalls.length; i += 1) {
+    colorBalls[i].addEventListener('click', (event) => {
+      const r = event.target.style.backgroundColor;
+      const s = `rgb${rgbColor.innerText}`;
+      const t = colorSelectionH2;
+      if (r === s) {
+        t.innerText = 'Acertou!';
+      } else if (r !== s) {
+        t.innerText = 'Errou! Tente novamente!';
+      }
+    });
+  }
+}
+checkAnswer();
+console.log(`"rgb${rgbColor.innerText}"`);
