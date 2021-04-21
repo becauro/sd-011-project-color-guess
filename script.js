8a08fec0cc5f1df7d697b2b6e57029eb92913e79
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-04-21 10:56:03 -0300

    Corrigindo exerc5 acertou miseravi

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // Exercicio 4 e 5
 let cores=[];
 let circulos = document.getElementsByClassName('ball');
-for(let i=0; i<circulos.length; i++){
+circulos[0].style.backgroundColor='rgb'+document.getElementById('rgb-color').innerText;
+
+for(let i=1; i<circulos.length; i++){
     let cor1 = Math.floor((Math.random()*100)+1);
     let cor2 = Math.floor((Math.random()*200)+1);
     let cor3 = Math.floor((Math.random()*300)+1);
