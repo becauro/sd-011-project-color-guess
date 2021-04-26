f1f48b4f59a0f2d5719fbe639f03ed9cf15d9c7a
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-04-26 20:27:15 -0300

    LINT + 10%

--- script.js
+++ script.js
@@ -1,59 +1,53 @@
-let corInicial='rgb(168, 34, 1)';
-document.getElementsByClassName('ball')[0].style.backgroundColor=corInicial;
-let score=0;
-document.getElementById('score').innerText=score;
+const corInicial = 'rgb(168, 34, 1)';
+document.getElementsByClassName('ball')[0].style.backgroundColor = corInicial;
+let score = 0;
+document.getElementById('score').innerText = score;
 
 // Exercicio 4 e 5
-function criarCores(){
-    let cores=[];
-    let circulos = document.getElementsByClassName('ball');
-    for(let i=1; i<circulos.length; i++){
-        let cor1 = Math.floor((Math.random()*100)+1);
-        let cor2 = Math.floor((Math.random()*200)+1);
-        let cor3 = Math.floor((Math.random()*300)+1);
-        circulos[i].style.backgroundColor ='rgb('+cor1+','+cor2+','+cor3+')';
-        cores[i]='rgb('+cor1+', '+cor2+', '+cor3+')';
-    }
+function criarCores() {
+  const circulos = document.getElementsByClassName('ball');
+  for (let i = 1; i < circulos.length; i += 1) {
+    const cor1 = Math.floor((Math.random() * 100) + 1);
+    const cor2 = Math.floor((Math.random() * 200) + 1);
+    const cor3 = Math.floor((Math.random() * 300) + 1);
+    circulos[i].style.backgroundColor = `rgb(${cor1},${cor2},${cor3})`;
+  }
 }
+
 criarCores();
 
 let resposta = false;
-let corComparar = 'rgb'+document.getElementById('rgb-color').innerText;
+let corComparar = `rgb${document.getElementById('rgb-color').innerText}`;
 
-let click = document.getElementById('balls'); // Quando clico em algum O
-click.addEventListener('click', function(e){
-    let corEscolhida = e.target.style.backgroundColor;
-    console.log('Cor Escolhida: '+corEscolhida);
-    console.log('Cor Comparar: '+ corComparar);
-
-    if(corEscolhida===corComparar){
-        resposta=true;
-    }
-    let textAnswer = document.getElementById('answer');
-    if(resposta){
-        textAnswer.innerHTML='Acertou!';
-        textAnswer.style.fontWeight='bold';
-        resposta = false;
-        score+=3;
-        document.getElementById('score').innerText=score;
-    }else{
-        textAnswer.innerHTML='Errou! Tente novamente!';
-        textAnswer.style.fontWeight='bold';
-        resposta = false;
-    }
-
-})
+const click = document.getElementById('balls'); // Quando clico em algum O
+click.addEventListener('click', (e) => {
+  const corEscolhida = e.target.style.backgroundColor;
+  if (corEscolhida === corComparar) { resposta = true; }
+  const textAnswer = document.getElementById('answer');
+  if (resposta) {
+    textAnswer.innerHTML = 'Acertou!';
+    textAnswer.style.fontWeight = 'bold';
+    resposta = false;
+    score += 3;
+    document.getElementById('score').innerText = score;
+  } else {
+    textAnswer.innerHTML = 'Errou! Tente novamente!';
+    textAnswer.style.fontWeight = 'bold';
+    resposta = false;
+  }
+});
 
 // Resetando
-//Exercicio 5
-let buttonReset = document.getElementById('reset-game');
-buttonReset.addEventListener('click', function(){
-    let c1 = Math.floor((Math.random()*100)+1);
-    let c2 = Math.floor((Math.random()*200)+1);
-    let c3 = Math.floor((Math.random()*300)+1);
-    document.getElementById('rgb-color').innerText='rgb('+c1+', '+c2+', '+c3+')';
-    document.getElementsByClassName('ball')[0].style.backgroundColor=document.getElementById('rgb-color').innerText;
-    corComparar = document.getElementById('rgb-color').innerText;
-    criarCores();
-    document.getElementById('answer').innerText='Escolha uma cor';
+// Exercicio 5
+const getBallStyle = document.getElementsByClassName('ball')[0];
+const buttonReset = document.getElementById('reset-game');
+buttonReset.addEventListener('click', () => {
+  const c1 = Math.floor((Math.random() * 100) + 1);
+  const c2 = Math.floor((Math.random() * 200) + 1);
+  const c3 = Math.floor((Math.random() * 300) + 1);
+  document.getElementById('rgb-color').innerText = `rgb(${c1}, ${c2}, ${c3})`;
+  getBallStyle.style.backgroundColor = document.getElementById('rgb-color').innerText;
+  corComparar = document.getElementById('rgb-color').innerText;
+  criarCores();
+  document.getElementById('answer').innerText = 'Escolha uma cor';
 });
